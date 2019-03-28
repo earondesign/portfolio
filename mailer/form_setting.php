637e115801e37157a9a30b1a79f5c98cf93a2a0d
earondesign <earondesign@gmail.com>
2019-03-28 12:26:30 -0700

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Portfolio Site'"; //letter subject
+	$to = 'earondesign@gmail.com'; // Enter Your E-mail
+	$from = 'admin@erikarincon.com'; // Admin e-mail
+	$fromName = 'Erika Rincon Portfolio'; // Your company name
+	$charset = 'UTF-8';
+?>
